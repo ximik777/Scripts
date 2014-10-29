76f2365988d0282ca86ac05be1e3a998edf3619c
Grigoriy Stratov <ya@ya.gs>
2014-10-29 13:04:36 +0300

    Create arr2xml.php

---
+++ xml/arr2xml.php
@@ -0,0 +1,9 @@
+<?
+function arr2xml($data, $i = -1){
+        $result = array(); $i++;
+        if($i===0) $result[] = '<?xml version="1.0" encoding="utf-8"?>';
+        foreach($data as $k => $v){
+            $result[] = str_repeat("\t", $i)."<".(is_numeric($k)?"item":$k)."".(is_array($v) && $v == array_values($v)?' list="true"':"").">".(is_array($v)?"\r\n".arr2xml($v, $i)."\r\n".str_repeat("\t", $i):$v)."</".(is_numeric($k)?"item":$k).">";
+        }
+        return implode("\r\n", $result);
+    }
